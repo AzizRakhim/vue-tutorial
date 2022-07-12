d0a24bf3bae050320a029dfd5a1303dc79c4f8bd
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-07-12 07:44:05 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <p>
+      Count: <strong>{{ count }}</strong>
+    </p>
+    <button class="btn btn-success" v-on:click="increment">Increment</button>
+    <button class="btn btn-primary" @click="decrement">Decrement</button>
+
+    <comment-form />
+    <comment-list v-bind:comments="comments" />
+  </div>
 </template>
 
+<script>
+import CommentForm from "./components/CommentForm";
+import CommentList from "./components/CommentList";
+export default {
+  components : {
+    CommentForm, 
+    CommentList
+  },
+  data() {
+    return {
+      message : "Hello Vue!",
+      count : 0,
+      comments : [
+        { id : 1, name : "Samar", email : "samar@gmail.com", content : "Lorem Ipsum content" },
+        { id : 2, name : "Yusuf", email : "yusuf@gmail.com", content : "Lorem Ipsum content" },
+        { id : 3, name : "Yunus", email : "yunus@gmail.com", content : "Lorem Ipsum content" }
+      ],
+    }
+  },
+  methods: {
+    increment(){
+      this.count++;
+    },
+    decrement(){
+      this.count--;
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  *{
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/components/CommentForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="w-75 mx-auto">
+      <h3>
+        Add Comments
+      </h3>
+      <form @submit.prevent="createComment">
+        <input 
+          v-model="name"
+          type="text" 
+          placeholder="Enter Your Name..." 
+          class="form-control"
+        >
+        <input 
+          v-model="email"
+          placeholder="Enter Your Email..." 
+          class="form-control my-3"
+        >
+        <input 
+          v-model="content"
+          type="text" 
+          placeholder="Enter Your Comment..." 
+          class="form-control"
+          >
+        <div class="mt-3 d-flex flex-row-reverse">
+          <button class="btn btn-primary">
+            Add Comments
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name : "",
+      email : "",
+      content : ""
+    }
+  },
+  methods : {
+    createComment() {
+      this.comment.id = Date.now();
+      this.comments.push(newComment);
+      this.name = "";
+      this.email = "";
+      this.content = "";
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
